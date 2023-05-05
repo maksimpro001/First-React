906fb65cb8d97d955cb7cf535170317aa22a3fda
maksimpro001 <ytmaksik425@gmail.com>
2023-05-05 16:59:21 +0300

    Update anime-db.js

--- src/assets/anime-db.js
+++ src/assets/anime-db.js
@@ -78,30 +78,6 @@ const anime_db = [
         duration: "24 Min.",
         genres: "Action, Adventure, Fantasy",
     },
-    {
-        name: "One-Piece",
-        id: 0,
-        img: img_1,
-        video: "https://www.youtube.com/embed/S8_YwFLCh4U",
-        description: "Gol D. Roger was known as the \"Pirate King,\" the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King.",
-        type: "Tv",
-        status: "Ongoing",
-        studio: "Toei Animation",
-        duration: "24 Min.",
-        genres: "Action, Adventure, Fantasy",
-    },
-    {
-        name: "One-Piece",
-        id: 0,
-        img: img_1,
-        video: "https://www.youtube.com/embed/S8_YwFLCh4U",
-        description: "Gol D. Roger was known as the \"Pirate King,\" the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King.",
-        type: "Tv",
-        status: "Ongoing",
-        studio: "Toei Animation",
-        duration: "24 Min.",
-        genres: "Action, Adventure, Fantasy",
-    },
 ]
 
 export default anime_db
